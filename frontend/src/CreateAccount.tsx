068e8f310adc3560516d05e54d8e5aec4ec6e2c0
ykomako4.9@gmail.com <ykomako4.9@gmail.com>
2023-12-18 03:15:19 -0500

    designed sign up page + error handling

--- frontend/src/CreateAccount.tsx
+++ frontend/src/CreateAccount.tsx
@@ -3,64 +3,140 @@ import { useState } from "react";
 const notahApi = "http://localhost:5245/api/v1/Accounts";
 
 export default function CreateAccount() {
-    const [display, setDisplay] = useState(false);
-    const [errMsg, setErrMsg] = useState("");
+  const [display, setDisplay] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [signupStatus, setSignupStatus] = useState("Sign Up");
 
-    const createAccount = () => {
-        const username = document.getElementById("username") as HTMLInputElement | null;
-        const email = document.getElementById("email") as HTMLInputElement | null;
-        const password = document.getElementById("password") as HTMLInputElement | null;
-        if (!(username && email && password)) {
-            setErrMsg("Failed to create account");
-            setDisplay(true);
-            return;
-        }
-        if (username.value && email.value && password.value) {
-            fetch(notahApi, {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                  },
-                body: JSON.stringify({username: username.value, email: email.value, password: password.value})
-            })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.status === 400) {
-                    setErrMsg("Username already exists");
-                    setDisplay(true);
-                    return;
-                }
-                window.location.href = "/login";
-            })
-            .catch(e => {
-                console.error(e);
-                setErrMsg("Failed to create account");
-                setDisplay(true);
-            })
-        }
-        else {
-            setErrMsg("Please fill out all fields");
-            setDisplay(true);
-            return;
-        }
+  const displayErrorMessage = (msg: string) => {
+    setErrMsg(msg);
+    setDisplay(true);
+    setTimeout(() => {
+      setDisplay(false);
+    }, 3000);
+  };
+
+  const createAccount = () => {
+    const username = document.getElementById(
+      "username"
+    ) as HTMLInputElement | null;
+    const email = document.getElementById("email") as HTMLInputElement | null;
+    const password = document.getElementById(
+      "password"
+    ) as HTMLInputElement | null;
+    const confirmPass = document.getElementById(
+      "confirm-password"
+    ) as HTMLInputElement | null;
+    if (!(username && email && password && confirmPass)) {
+      displayErrorMessage("*Failed to create account");
+      return;
     }
-    return (
-        <>
-        <div className="bg-amber-50 w-1/2 m-auto">
-            <div className="flex items-center">
-                <p className="text-red-600" style={{display: display? "block" : "none"}}>{errMsg}</p>
-                <label htmlFor="username">Username</label>
-                <input className="border-2" id="username" type="text" placeholder="Username"/>
-                <label htmlFor="email">Email</label>
-                <input className="border-2" id="email" type="text" placeholder="Email" />
-                <label htmlFor="password">Password</label>
-                <input className="border-2" id="password" type="password" placeholder="Password"/>
+    if (
+      !(username.value && email.value && password.value && confirmPass.value)
+    ) {
+      displayErrorMessage("*Please fill out all fields");
+      return;
+    }
+    if (password.value !== confirmPass.value) {
+      displayErrorMessage("*Password does not match");
+      return;
+    }
+    setSignupStatus("Signing up...");
+    fetch(notahApi, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username.value,
+        email: email.value,
+        password: password.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSignupStatus("Sign Up");
+        console.log(data);
+        if (data.status === 400) {
+          displayErrorMessage("*Username already exists");
+          return;
+        }
+        window.location.href = "/login";
+      })
+      .catch((e) => {
+        setSignupStatus("Sign Up");
+        console.error(e);
+        displayErrorMessage("*Failed to create account");
+      });
+  };
+  return (
+    <>
+      <div className="bg-amber-100 h-screen flex justify-center pt-10">
+        <div className="bg-white p-2 rounded-xl h-fit min-w-[400px]">
+          <h2 className="mt-5 mb-5 text-center font-semibold text-2xl">
+            Sign Up
+          </h2>
+          <p
+            className="text-red-600 ml-5"
+            style={{ display: display ? "block" : "none" }}
+          >
+            {errMsg}
+          </p>
+          <div className="grid grid-cols-1 gap-4 place-content-center ml-5 mr-5">
+            <div className="grid grid-cols-1 content-start gap-1">
+              <label htmlFor="username">Username</label>
+              <input
+                id="username"
+                type="text"
+                placeholder="Type your username"
+              />
+              <hr />
             </div>
-            <button className="border-2" onClick={() => createAccount()}>Create Account</button>
-            <a href="/login">back to login</a>
+            <div className="grid grid-cols-1 content-start gap-1">
+              <label htmlFor="email">Email</label>
+              <input id="email" type="email" placeholder="Type your email" />
+              <hr />
+            </div>
+            <div className="grid grid-cols-1 content-start gap-1">
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                placeholder="Type your password"
+              />
+              <hr />
+            </div>
+            <div className="grid grid-cols-1 content-start gap-1">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input
+                id="confirm-password"
+                type="password"
+                placeholder="Confirm your password"
+              />
+              <hr />
+            </div>
+            <button
+              className="mt-3 rounded-lg p-2 font-medium hover:bg-amber-200 active:bg-amber-300 bg-amber-100 "
+              onClick={() => createAccount()}
+            >
+              {signupStatus}
+            </button>
+            <div className="mt-5 text-center flex justify-center">
+              <p>Already Have an Account?&nbsp;</p>
+              <a
+                className="text-center text-blue-600 font-semibold hover:underline"
+                href="/login"
+              >
+                Login
+              </a>
+            </div>
+            <hr />
+            <a className="text-center hover:underline mb-5" href="/">
+              Return To Home
+            </a>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { actionCreators } from "./store";
 import { bindActionCreators } from "@reduxjs/toolkit";
@@ -16,6 +17,17 @@ type Account = {
 export default function Login() {
   const dispatch = useDispatch();
   const { setAccount } = bindActionCreators(actionCreators, dispatch);
+  const [display, setDisplay] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [loginStatus, setLoginStatus] = useState("Login");
+
+  const displayErrorMessage = (msg: string) => {
+    setErrMsg(msg);
+    setDisplay(true);
+    setTimeout(() => {
+      setDisplay(false);
+    }, 3000);
+  };
 
   const login = () => {
     const email = document.getElementById("email") as HTMLInputElement | null;
@@ -23,42 +35,47 @@ export default function Login() {
       "password"
     ) as HTMLInputElement | null;
     if (!(email && password)) {
+      displayErrorMessage("*Failed to login");
       return;
     }
-    if (email.value && password.value) {
-      fetch(notahApi, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email: email.value, password: password.value }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.status === 404) throw new Error();
-          else return data as Account;
-        })
-        .then((data) => {
-          console.log(data);
-          setAccount({
-            id: data.id,
-            username: data.username,
-            access: "user",
-            dateCreated: data.dateCreated,
-            lastEdited: data.lastEdited,
-          });
-          window.location.href = `/${data.username}`;
-        })
-        .catch((e) => {
-          console.log("failed to login");
-          console.error(e);
+    if (!(email.value && password.value)) {
+      displayErrorMessage("*Please fill out all fields");
+      return;
+    }
+    setLoginStatus("Logging in...");
+    fetch(notahApi, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email.value, password: password.value }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLoginStatus("Login");
+        if (data.status === 404) {
+          displayErrorMessage("*Email or password is incorrect");
+          return;
+        } else {
+          data = data as Account;
+        }
+        console.log(data);
+        setAccount({
+          id: data.id,
+          username: data.username,
+          access: "user",
+          dateCreated: data.dateCreated,
+          lastEdited: data.lastEdited,
         });
-    } else {
-      console.log("Please fill out all fields");
-      return;
-    }
+        window.location.href = `/${data.username}`;
+      })
+      .catch((e) => {
+        setLoginStatus("Login");
+        console.error(e);
+        displayErrorMessage("*Failed to login");
+      });
   };
   return (
     <>
@@ -67,6 +84,12 @@ export default function Login() {
           <h2 className="mt-5 mb-5 text-center font-semibold text-2xl">
             Login
           </h2>
+          <p
+            className="text-red-600 ml-5"
+            style={{ display: display ? "block" : "none" }}
+          >
+            {errMsg}
+          </p>
           <div className="grid grid-cols-1 gap-4 place-content-center ml-5 mr-5">
             <div className="grid grid-cols-1 content-start gap-1">
               <label htmlFor="email">Email</label>
@@ -92,12 +115,17 @@ export default function Login() {
               className="mt-3 rounded-lg p-2 font-medium hover:bg-amber-200 active:bg-amber-300 bg-amber-100 "
               onClick={() => login()}
             >
-              Login
+              {loginStatus}
             </button>
-            <div className="mt-5 text-center">Or Sign Up Using</div>
-            <a className="text-center hover:underline" href="/create-account">
-              SIGN UP
-            </a>
+            <div className="mt-5 text-center flex justify-center">
+              <p>New to Notah?&nbsp;</p>
+              <a
+                className="text-center text-blue-600 font-semibold hover:underline"
+                href="/create-account"
+              >
+                Sign Up
+              </a>
+            </div>
             <hr />
             <a className="text-center hover:underline mb-5" href="/">
               Return To Home
