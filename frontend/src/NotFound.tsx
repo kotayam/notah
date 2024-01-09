1e9cc47a5048447e8c0b55dc165c87387a4df7ba
ykomako4.9@gmail.com <ykomako4.9@gmail.com>
2024-01-09 17:53:06 +0900

    not found page styled

--- frontend/src/NotFound.tsx
+++ frontend/src/NotFound.tsx
@@ -1,8 +1,17 @@
 export default function NotFound() {
-    return (
-        <div>
-            <p>404 Page Not Found</p>
-            <a href="/">Return to home</a>
-        </div>
-    )
+  return (
+    <div className="bg-gradient-to-br from-yellow-200 via-amber-600 to-red-600 bg-[length:400%_400%] min-h-[100vh] flex justify-center items-center animate-gradient text-lg mobile:text-md">
+      <div className="grid grid-cols-1 gap-2 place-content-center text-center text-white">
+        <h1 className="text-2xl mobile:text-xl">404 Page Not Found</h1>
+        <img
+          src="/notah-logo.gif"
+          alt="Notah Logo"
+          className="w-80 mobile:w-40"
+        />
+        <a href="/" className="hover:underline">
+          Return to home
+        </a>
+      </div>
+    </div>
+  );
 }
