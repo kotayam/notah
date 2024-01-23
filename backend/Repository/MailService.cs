95ee4791df90bd4e8fbeafd8b688d7ab13b7e36f
ykomako4.9@gmail.com <ykomako4.9@gmail.com>
2024-01-23 09:45:51 -0500

    mail template as string

--- backend/Repository/MailService.cs
+++ backend/Repository/MailService.cs
@@ -27,43 +27,163 @@ namespace backend.Repository
                 var email = new MimeMessage();
                 email.From.Add(MailboxAddress.Parse(_mailSettings.SenderName));
                 email.To.Add(MailboxAddress.Parse(mailData.EmailToAddress));
-                
-                string filePath = "";
+
                 string templateText = "";
                 string subject = "";
                 var emailBodyBuilder = new BodyBuilder();
-                var image = emailBodyBuilder.LinkedResources.Add("/app/MailTemplates/notah-logo.gif");
-                image.ContentId = MimeUtils.GenerateMessageId();
                 switch(mailData.EmailPurpose) {
                     case "signup": 
                         subject = "Welcome to Notah!";
-                        filePath = "/app/MailTemplates/Signup.html";
-                        templateText = File.ReadAllText(filePath);
-                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName, image.ContentId);
+                        templateText = @"<!DOCTYPE html>
+                        <html>
+                        <head>
+                        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                        <title>Welcome to Notah</title>
+                        </head>
+                        <body style='font-family: sans-serif; background-color: lightgray;'>
+                        <div style='display: grid;grid-column: 1;place-content: center;text-align: center;'>
+                        <h1 style='font-weight: 500; font-size: x-large'>Hello {0}!</h1>
+                        <div>
+                            <img src='https://i.postimg.cc/2SVnhXpG/notah-logo.gif' alt='Notah Logo' style='width: 200px'>
+                        </div>
+                        <p>Thank you for signing up for Notah! Start taking your notes now!</p>
+                        <a
+                            href='https://notah.heppoko.space/login'
+                            style='text-decoration: underline'
+                            >Login to Notah</a
+                        >
+                        </div>
+                        </body>
+                        </html>";
+                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName);
                         break;
                     case "login":
                         subject = "Successful Login to Notah";
-                        filePath = "/app/MailTemplates/Login.html";
-                        templateText = File.ReadAllText(filePath);
-                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName, image.ContentId, DateTime.UtcNow.ToString("ddd, dd MMM yyy HH:mm:ss 'GMT'"));
+                        templateText = @"<!DOCTYPE html>
+                        <html>
+                        <head>
+                            <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                            <title>Successful Sign-in</title>
+                        </head>
+                        <body style='font-family: sans-serif; background-color: lightgray;'>
+                            <div
+                            style='
+                                display: grid;
+                                grid-column: 1;
+                                place-content: center;
+                                text-align: center;
+                            '
+                            >
+                            <h1 style='font-weight: 500; font-size: x-large'>Welcome back {0}!</h1>
+                            <div>
+                                <img src='https://i.postimg.cc/2SVnhXpG/notah-logo.gif' alt='Notah Logo' style='width: 200px'>
+                            </div>
+                            <p>Successfully logged in at {1}.</p>
+                            <a
+                                href='https://notah.heppoko.space'
+                                style='text-decoration: underline'
+                                >Notah Home Page</a
+                            >
+                            </div>
+                        </body>
+                        </html>";
+                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName, DateTime.UtcNow.ToString("ddd, dd MMM yyy HH:mm:ss 'GMT'"));
                         break;
                     case "account-delete":
                         subject = "Successfully Deleted Your Account";
-                        filePath = "/app/MailTemplates/Delete.html";
-                        templateText = File.ReadAllText(filePath);
-                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName, image.ContentId);
+                        templateText = @"<!DOCTYPE html>
+                        <html>
+                        <head>
+                            <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                            <title>Successfully Deleted Account</title>
+                        </head>
+                        <body style='font-family: sans-serif; background-color: lightgray;'>
+                            <div
+                            style='
+                                display: grid;
+                                grid-column: 1;
+                                place-content: center;
+                                text-align: center;
+                            '
+                            >
+                            <h1 style='font-weight: 500; font-size: x-large'>{0}, thank you for using Notah!</h1>
+                            <div>
+                                <img src='https://i.postimg.cc/2SVnhXpG/notah-logo.gif' alt='Notah Logo' style='width: 200px'>
+                            </div>
+                            <p>Your account has been deleted. Thank you for using Notah!</p>
+                            <a
+                                href='https://notah.heppoko.space'
+                                style='text-decoration: underline'
+                                >Notah Home Page</a
+                            >
+                            </div>
+                        </body>
+                        </html>";
+                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName);
                         break;
                     case "account-update":
                         subject = "Successfully Updated Your Account";
-                        filePath = "/app/MailTemplates/Update.html";
-                        templateText = File.ReadAllText(filePath);
-                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName, image.ContentId, DateTime.UtcNow.ToString("ddd, dd MMM yyy HH:mm:ss 'GMT'"));
+                        templateText = @"<!DOCTYPE html>
+                        <html>
+                        <head>
+                            <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                            <title>Successfully Updated Account</title>
+                        </head>
+                        <body style='font-family: sans-serif; background-color: lightgray;'>
+                            <div
+                            style='
+                                display: grid;
+                                grid-column: 1;
+                                place-content: center;
+                                text-align: center;
+                            '
+                            >
+                            <h1 style='font-weight: 500; font-size: x-large'>{0}, your account information has been updated.</h1>
+                            <div>
+                                <img src='https://i.postimg.cc/2SVnhXpG/notah-logo.gif' alt='Notah Logo' style='width: 200px'>
+                            </div>
+                            <p>Your username or email has been updated at {1}</p>
+                            <a
+                                href='https://notah.heppoko.space/login'
+                                style='text-decoration: underline'
+                                >Login to Notah</a
+                            >
+                            </div>
+                        </body>
+                        </html>";
+                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName, DateTime.UtcNow.ToString("ddd, dd MMM yyy HH:mm:ss 'GMT'"));
                         break;
                     case "password-change":
                         subject = "Successfully Changed Your Password";
-                        filePath = "/app/MailTemplates/Password.html";
-                        templateText = File.ReadAllText(filePath);
-                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName, image.ContentId, DateTime.UtcNow.ToString("ddd, dd MMM yyy HH:mm:ss 'GMT'"));
+                        templateText = @"<!DOCTYPE html>
+                        <html>
+                        <head>
+                            <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                            <title>Successfully Changed Your Password</title>
+                        </head>
+                        <body style='font-family: sans-serif; background-color: lightgray;'>
+                            <div
+                            style='
+                                display: grid;
+                                grid-column: 1;
+                                place-content: center;
+                                text-align: center;
+                            '
+                            >
+                            <h1 style='font-weight: 500; font-size: x-large'>{0}, your password has been changed.</h1>
+                            <div>
+                                <img src='https://i.postimg.cc/2SVnhXpG/notah-logo.gif' alt='Notah Logo' style='width: 200px'>
+                            </div>
+                            <p>Your account password has been updated at {1}</p>
+                            <a
+                                href='https://notah.heppoko.space/login'
+                                style='text-decoration: underline'
+                                >Login to Notah</a
+                            >
+                            </div>
+                        </body>
+                        </html>";
+                        emailBodyBuilder.HtmlBody = string.Format(templateText, mailData.EmailToName, DateTime.UtcNow.ToString("ddd, dd MMM yyy HH:mm:ss 'GMT'"));
                         break;
                 }
                 email.Subject = subject;
